c4d450ed3512b7574eedd8fdfe8067a1b1075849
toshimaru <me@toshimaru.net>
2023-11-12 10:42:20 +0900

    Add `SDoc::Helpers#method_source_code_and_url`

--- lib/sdoc/helpers.rb
+++ lib/sdoc/helpers.rb
@@ -24,12 +24,13 @@ module SDoc::Helpers
     end
   end
 
-protected
-  def group_name name
-    if match = name.match(/^([a-z])/i)
-      match[1].upcase
-    else
-      '#'
+  def method_source_code_and_url(rdoc_method)
+    source_code = rdoc_method.tokens_to_s if rdoc_method.token_stream
+
+    if source_code&.match(/File\s(\S+), line (\d+)/)
+      source_url = github_url(Regexp.last_match(1), line: Regexp.last_match(2))
     end
+
+    [rdoc_method.instance_of?(RDoc::GhostMethod) ? nil : source_code, source_url]
   end
 end
