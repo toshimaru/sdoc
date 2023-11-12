7391aa23fbc307c8fac95a0cbbc2a9bb8d49159a
Toshimaru <me@toshimaru.net>
2023-11-12 10:55:18 +0900

    Updates to show GitHub source url (#5)
    * Remove `SDoc::GitHub` module
    
    * Create `SDoc::Helpers::Git` module
    
    * Add `SDoc::Helpers#method_source_code_and_url`

---
+++ lib/sdoc/helpers/git.rb
@@ -0,0 +1,51 @@
+module SDoc::Helpers::Git
+  def github_url(relative_path, line: nil)
+    return unless github?
+    line = "#L#{line}" if line
+    "https://github.com/#{github_repository}/blob/#{git_head_sha1}/#{relative_path}#{line}"
+  end
+
+  def github?
+    @options.github && git? && github_repository
+  end
+
+  attr_writer :git_repo_path
+
+  def git?
+    @git_repo_path ||= Dir.chdir(@options.root) { `git rev-parse --show-toplevel 2> /dev/null`.chomp }
+    !@git_repo_path.empty?
+  end
+
+  def git_command(command)
+    Dir.chdir(@options.root) { `git #{command}`.chomp } if git?
+  end
+
+  attr_writer :git_head_branch
+
+  def git_head_branch
+    @git_head_branch ||= git_command("rev-parse --abbrev-ref HEAD")
+  end
+
+  attr_writer :git_head_sha1
+
+  def git_head_sha1
+    @git_head_sha1 ||= git_command("rev-parse HEAD")
+  end
+
+  attr_writer :git_head_timestamp
+
+  def git_head_timestamp
+    @git_head_timestamp ||= git_command("show -s --format=%cI HEAD")
+  end
+
+  attr_writer :git_origin_url
+
+  def git_origin_url
+    @git_origin_url ||= git_command("config --get remote.origin.url")
+  end
+
+  def github_repository
+    return @github_repository if defined?(@github_repository)
+    @github_repository = git_origin_url.chomp(".git")[%r"github\.com[/:](.+)", 1]
+  end
+end
