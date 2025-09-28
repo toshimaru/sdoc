e289db22e80c2f3e8d6a249ce848fa5470aa9382
Toshimaru <me@toshimaru.net>
2025-09-28 18:24:40 +0900

    refactor: Remove strip_tags and truncate

--- lib/sdoc/helpers.rb
+++ lib/sdoc/helpers.rb
@@ -8,28 +8,6 @@ module SDoc::Helpers
   require_relative "helpers/git"
   include ::SDoc::Helpers::Git
 
-  # Strips out HTML tags from a given string.
-  #
-  # Example:
-  #
-  #   strip_tags("<strong>Hello world</strong>") => "Hello world"
-  def strip_tags(text)
-    text.gsub(%r{</?[^>]+?>}, "")
-  end
-
-  # Truncates a given string. It tries to take whole sentences to have
-  # a meaningful description for SEO tags.
-  #
-  # The only available option is +:length+ which defaults to 200.
-  def truncate(text, options = {})
-    if text
-      length = options.fetch(:length, 200)
-      stop   = text.rindex(".", length - 1) || length
-
-      "#{text[0, stop]}."
-    end
-  end
-
   def method_source_code_and_url(rdoc_method)
     source_code = h(rdoc_method.tokens_to_s) if rdoc_method.token_stream
 
